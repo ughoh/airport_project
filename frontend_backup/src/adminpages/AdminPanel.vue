881ffb1cf7e02652fe944222e17ed74ae4271b92
Solomiya <solomiya7512@gmail.com>
2025-06-13 09:03:10 +0300

    Ignore frontend_backup folder

--- frontend_backup/src/adminpages/AdminPanel.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import AdminHeader from "@/adminpages/AdminHeader.vue";
-</script>
-
-<template>
-  <div class="w-full min-h-screen bg-indigo-100 pb-12">
-    <AdminHeader />
-  </div>
-</template>
--- frontend_backup/src/components/Info/AboutAirport/AirportScheme.vue
+++
@@ -1,6 +0,0 @@
-<template>
-<div class="p-6 bg-white rounded-lg shadow max-w-4xl mx-auto">
-    <h2 class="text-3xl font-bold mb-4 text-indigo-400">Skylight International Airport scheme</h2>
-    <img src="/scheme_air.png" alt="Airport scheme">
-</div>
-</template>
